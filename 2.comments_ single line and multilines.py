6ce7f56dc67dae74ede9d954007a7e63fe8dc9d9
osama-coder <osamakort63@gmail.com>
2022-03-28 14:59:51 +0200

    second lesson

---
+++ 2.comments_ single line and multilines.py
@@ -0,0 +1,11 @@
+# عمل تعليق لسطر واحد نستخدم رمز ال"#"
+
+# comment 1
+# comment 2
+
+# عمل تعليق لأكثر من سطر نضعهم بين علامتي ال'''comment lines'''
+
+'''comment line number 1
+comment line number 2
+comment line number 3
+'''
