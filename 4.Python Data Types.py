404922d9f6f7c265f02ec674b30a4aa794c97f64
osama-coder <osamakort63@gmail.com>
2022-03-28 15:33:02 +0200

    fourth lesson

---
+++ 4.Python Data Types.py
@@ -0,0 +1,11 @@
+# هناك أنواع مختلفة من المتغيرات كما يلي
+x1 = "Hello World"	# str
+x2 = 20	# int
+x3 = 20.5	# float
+x4 = ["apple", "banana", "cherry"]	# list
+x5 = ("apple", "banana", "cherry")	# tuple
+x6 = range(6)	# range
+x7 = {"name" : "John", "age" : 36}	# dict
+x8 = {"apple", "banana", "cherry"}	# set
+x9 = True	# bool
+x10 = b"Hello"	# bytes
