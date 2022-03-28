eeee8938df5c2f223f51795cbbee10d63eb0f217
osama-coder <64306255+osama-coder@users.noreply.github.com>
2022-03-28 15:00:50 +0200

    Delete 1.outpout and input from cmd.py

--- 1.outpout and input from cmd.py
+++
@@ -1,8 +0,0 @@
-## cmdطباعة البيانات علي ال
-
-print("osama")  # إخراج نص
-print(123)    # إخراج رقم
-
-#إدخال بيانات للبرنامج من الcmd #
-x = input("message before the input data :")  #إستقبال بيانات في المتغير x
-y = input("enter your name: ")
